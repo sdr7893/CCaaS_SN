91dfbac6a56474de3a68113d1661cc914491e4e4
sdr7893 <93908410+sdr7893@users.noreply.github.com>
2024-10-12 11:42:02 +0530

    Update SNExampleCTIDriver.js

--- SNExampleCTIDriver.js
+++ SNExampleCTIDriver.js
@@ -1,212 +1,66 @@
-(() => {
-  ["use strict"];
-  ({
-    973: function (e, n) {
-      var t =
-        (this && this.__awaiter) ||
-        function (e, n, t, o) {
-          return new (t || (t = Promise))(function (c, r) {
-            function i(e) {
-              try {
-                a(o.next(e));
-              } catch (e) {
-                r(e);
-              }
-            }
-            function s(e) {
-              try {
-                a(o.throw(e));
-              } catch (e) {
-                r(e);
-              }
-            }
-            function a(e) {
-              var n;
-              e.done
-                ? c(e.value)
-                : ((n = e.value),
-                  n instanceof t
-                    ? n
-                    : new t(function (e) {
-                        e(n);
-                      })).then(i, s);
-            }
-            a((o = o.apply(e, n || [])).next());
-          });
-        };
-      Object.defineProperty(n, "__esModule", { value: !0 }),
-        (n.CUSTOMERDATAENUM = void 0),
-        (n.CUSTOMERDATAENUM = {
-          PHONENUMBER: "phonenumber",
-          EMAIL: "email",
-          NAME: "name",
-        });
-      const o = "Contact",
-        c = "Account";
-      class r {
-        constructor(e) {
-          if (!e)
-            throw new Error("ccaaSSDKInstance cannot be null or undefined");
-          this.ccaaSSDKInstance = e;
-        }
-        initialize() {
-          if (void 0 === window.sforce || void 0 === window.sforce.opencti) {
-            const e = "/support/api/54.0/lightning/opencti.js",
-              n = `${new URL(document.referrer).origin}${e}`;
-            return r.loadScript(n);
-          }
-          return Promise.resolve(!0);
-        }
-        conversationReady(e) {
-          return t(this, void 0, void 0, function* () {
-            return new Promise((n, o) =>
-              t(this, void 0, void 0, function* () {
-                const n =
-                  yield this.ccaaSSDKInstance.conversation.getConversationData(
-                    e.conversationId,
-                    ["msdyn_isoutbound", "msdyn_channel", "subject"]
-                  );
-                let t = window.sforce.opencti.CALL_TYPE.INBOUND;
-                n.msdyn_channel &&
-                  ((19237e4 !== Number(n.msdyn_channel) &&
-                    19244e4 !== Number(n.msdyn_channel)) ||
-                    (n.msdyn_isoutbound &&
-                      (t = window.sforce.opencti.CALL_TYPE.OUTBOUND)));
-                const { contactId: o, accountId: c } =
-                  yield r.getContactOrAccountId(e, t);
-                (this.contactId = o),
-                  (this.accountId = c),
-                  (this.contactId || this.accountId) &&
-                    r.screenPop(this.contactId || this.accountId);
-              })
-            );
-          });
-        }
-        endConversation() {
-          return t(this, void 0, void 0, function* () {
-            throw new Error("Method is not defined.");
-          });
-        }
-        onClickToDial(e) {
-          window.sforce.opencti.enableClickToDial(),
-            window.sforce.opencti.onClickToDial({
-              listener: (n) => {
-                const t = {
-                  number: n.number,
-                  objectType: n.objectType,
-                  recordId: n.recordId,
-                  recordName: n.recordName,
-                };
-                e(t), this.setSoftPhonePanelVisibility(!0);
-              },
-            });
-        }
-        setSoftPhonePanelVisibility(e = !0) {
-          e !==
-            window.sforce.opencti.isSoftphonePanelVisible({
-              callback: (e) => {
-                if (e.success) return e.returnValue.visible;
-                throw new Error(e.errors);
-              },
-            }) &&
-            window.sforce.opencti.setSoftphonePanelVisibility({ visible: e });
-        }
-        setSoftPhonePanelWidth(e) {
-          window.sforce.opencti.setSoftphonePanelWidth({ widthPX: e });
-        }
-        setSoftPhonePanelHeight(e) {
-          window.sforce.opencti.setSoftphonePanelHeight({ heightPX: e });
-        }
-        static screenPop(e) {
-          return new Promise((n, t) => {
-            window.sforce.opencti.screenPop({
-              type: window.sforce.opencti.SCREENPOP_TYPE.SOBJECT,
-              params: { recordId: e },
-              callback: (e) => {
-                e.success ? n(e.success) : t(e.errors);
-              },
-            });
-          });
-        }
-        static searchForRecords(e, n) {
-          return t(this, void 0, void 0, function* () {
-            return new Promise((t, r) => {
-              window.sforce.opencti.searchAndScreenPop({
-                searchParams: e,
-                callType: n,
-                deferred: !0,
-                callback: (e) => {
-                  const n = [];
-                  if (e.success && e.returnValue) {
-                    for (const [t, r] of Object.entries(e.returnValue))
-                      !r ||
-                        (r.RecordType !== o && r.RecordType !== c) ||
-                        n.push({ id: t, type: r.RecordType });
-                    t(n);
-                  } else r(e.errors);
-                },
-              });
-            });
-          });
-        }
-        static getContactOrAccountId(e, n) {
-          return t(this, void 0, void 0, function* () {
-            const t = new Set(),
-              i = new Set(),
-              s = yield r.getRecords(e, n);
-            return (
-              s &&
-                s.forEach(({ id: e, type: n }) => {
-                  n === o ? t.add(e) : n === c && i.add(e);
-                }),
-              {
-                contactId: 1 === t.size ? t.values().next().value : void 0,
-                accountId: 1 === i.size ? i.values().next().value : void 0,
-              }
-            );
-          });
-        }
-        static getRecords(e, o) {
-          return t(this, void 0, void 0, function* () {
-            const t = e.customerData;
-            let c = [];
-            try {
-              return (
-                r.checkIfNotEmtpy(n.CUSTOMERDATAENUM.PHONENUMBER, t)
-                  ? (c = yield r.searchForRecords(t.phonenumber, o))
-                  : r.checkIfNotEmtpy(n.CUSTOMERDATAENUM.EMAIL, t)
-                  ? (c = yield r.searchForRecords(t.email, o))
-                  : r.checkIfNotEmtpy(n.CUSTOMERDATAENUM.NAME, t) &&
-                    (c = yield r.searchForRecords(t.name, o)),
-                c
-              );
-            } catch (e) {
-              throw new Error(e);
-            }
-          });
-        }
-        static checkIfNotEmtpy(e, n) {
-          return void 0 !== n[e] && null !== n[e] && "" !== n[e];
-        }
-        static loadScript(e) {
-          return new Promise((n, t) => {
-            const o = document.createElement("script");
-            (o.type = "text/javascript"),
-              (o.async = !0),
-              (o.src = e),
-              (o.onload = function () {
-                n(!0);
-              }),
-              (o.onerror = function () {
-                t(new Error(`Error in loading ${e}`));
-              }),
-              document.getElementsByTagName("head")[0].appendChild(o);
-          });
-        }
-      }
-      (window.CCaaS = window.CCaaS || {}),
-        window.CCaaS.CTIDriver || (window.CCaaS.CTIDriver = r),
-        (n.default = r);
-    },
-  })[973](0, {});
-})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/SNExampleCTIDriver.ts":
+/*!***********************************!*\
+  !*** ./src/SNExampleCTIDriver.ts ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {\n    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }\n    return new (P || (P = Promise))(function (resolve, reject) {\n        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }\n        function rejected(value) { try { step(generator[\"throw\"](value)); } catch (e) { reject(e); } }\n        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }\n        step((generator = generator.apply(thisArg, _arguments || [])).next());\n    });\n};\nclass SNExampleCTIDriver {\n    /**\n     * Constructor\n     *\n     * @param {CCaaSSdk} ccaaSSDKInstance Instance of CCaaSSDK\n     *\n     * @returns Instance\n     */\n    constructor(ccaaSSDKInstance) {\n        if (!ccaaSSDKInstance) {\n            throw new Error('ccaaSSDKInstance cannot be null or undefined');\n        }\n        this.ccaaSSDKInstance = ccaaSSDKInstance;\n    }\n    /**\n     * Function to initialize the scripts and do the operations once it is loaded\n     *\n     * @returns Promise void\n     */\n    initialize() {\n        let isServiceNowPresent = typeof window.openFrameAPI !== 'undefined';\n        if (isServiceNowPresent === false) {\n            const openCTIurlPath = `/scripts/openframe/latest/openFrameAPI.min.js`;\n            const servicenoworgdomain = window.location.ancestorOrigins[0];\n            const source = `${servicenoworgdomain}${openCTIurlPath}`;\n            const sourceResult = SNExampleCTIDriver.loadScript(source);\n            return sourceResult;\n        }\n        return Promise.resolve(true);\n    }\n    /**\n     * Funtion to set width of softphone panel\n     *\n     * @param {number} width number to set width\n     *\n     * @returns void\n     */\n    setSoftPhonePanelWidth(width) {\n        window.openFrameAPI.setWidth(width);\n    }\n    /**\n    * Funtion to set height of softphone panel\n    *\n    * @param {number} height number to set height\n    *\n    * @returns void\n    */\n    setSoftPhonePanelHeight(height) {\n        window.openFrameAPI.setHeight(height);\n    }\n    /**\n     * Function to set visibility of softphone panel\n     *\n     * @param {boolean} visible value to set visibility\n     *\n     * @returns void\n     */\n    setSoftPhonePanelVisibility(visible) {\n        if (visible) {\n            window.openFrameAPI.show();\n        }\n        else {\n            window.openFrameAPI.hide();\n        }\n    }\n    /**\n     * Prepares a conversation, if an account/contact is found then it shows screen pop with customer information\n     * @param {ConversationInfo} conversationData Object containing conversation details and customer data.\n     *\n     * @returns void\n     */\n    conversationReady(conversationData) {\n        return __awaiter(this, void 0, void 0, function* () {\n            return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {\n                // TO IMPLEMENT.\n                console.log(conversationData);\n                reject(new Error('Customer data for search is missing'));\n            }));\n        });\n    }\n    /**\n     * Function for ending a conversation.\n     *\n     * @param {string} conversationId - Unique identifier of the conversation.\n     * @returns void\n     */\n    endConversation(conversationId) {\n        throw new Error('Method not implemented.');\n    }\n    /**\n    * Function to handle operations on Click to dial\n    * @param {ClickToDialCallbackFunction} callbackFuntion func to be registered for click-to-dial.\n    *\n    * @returns void\n    */\n    onClickToDial(callbackFuntion) {\n        throw new Error('Method not implemented.');\n    }\n    /**\n     * Helper function to load the snctidriver script\n     * @param source\n     * @returns Promise fulilled with boolean on whether script is loaded or not\n     */\n    static loadScript(source) {\n        return new Promise((resolve, reject) => {\n            const script = document.createElement('script');\n            script.type = 'text/javascript';\n            script.async = true;\n            script.src = source;\n            script.onload = function () {\n                resolve(true);\n            };\n            script.onerror = function () {\n                reject(new Error(`Error in loading ${source}`));\n            };\n            document.getElementsByTagName('head')[0].appendChild(script);\n        });\n    }\n}\nwindow.CCaaS = window.CCaaS || {};\nif (!window.CCaaS.CTIDriver) {\n    window.CCaaS.CTIDriver = SNExampleCTIDriver;\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (SNExampleCTIDriver);\n\n\n//# sourceURL=webpack://sn-example-ctidriver/./src/SNExampleCTIDriver.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/SNExampleCTIDriver.ts"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
